6228e3cabe571fceb80a634731253abe6d13ec5b
keith hill <keith.hill@metastock.com>
2022-07-04 10:28:57 -0600

    pages, renaming

---
+++ pages/GSMS.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    getting started with metastock
+  </section>
+</template>
+
+<script>
+/* ✓ GOOD */
+export default {
+  name: 'GSMS',
+  // ...
+}
+</script>
+</script>
+
+<style>
+</style>
